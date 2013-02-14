e7d6b9a74f7878930d9351e027a97c6b117a6f04
Brian Graves <git@brianleegraves.com>
2013-02-13 21:29:17 -0600

    Bundle Updates

--- src/config.rb
+++ src/config.rb
@@ -48,14 +48,14 @@
 # end
 set :css_dir, 'stylesheets'
 
-set :js_dir, 'javascripts'
+#set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
   # activate :minify_javascript
@@ -64,7 +64,7 @@ configure :build do
   # activate :cache_buster
 
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
 
   # Compress PNGs after build
   # First: gem install middleman-smusher
